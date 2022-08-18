0caa695d2f8ed5f9fec5809c41379642cd3ca1b0
ola <alejar5188@gmail.com>
2022-08-18 13:39:58 +0200

    Dashboard

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
+//Przeskrolowanie do interesującego elementu
 function smoothScroll(element){
     document.querySelector(element).scrollIntoView(
-        {behavior:'smooth'}
-    )
+        {
+            behavior:'smooth'
+        }
+    );
+}
+window.onscroll = function(){
+scroll();
+}
+function scroll(){
+    if(document.body.scrollTop >30 || document.documentElement.scrollTop >30 ){
+        document.getElementById('up-button').style.display="block";
+    } else {
+        document.getElementById('up-button').style.display="none";
+    }
+}
+
+function scrollTop(){
+    smoothScroll('header');
 }
